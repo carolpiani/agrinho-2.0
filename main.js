3ae8adebc854d6400c0d80900f734a01530a1aa1
carolpiani <piani.carolina@escola.pr.gov.br>
2026-07-29 08:45:13 -0300

    ..

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const tempos = [
-    new Date("2026-11-30T00:00:00"),
-    new Date("2026-12-05T00:00:00"),
+    new Date("2026-12-18T00:00:00"),
+    new Date("2026-12-31T00:00:00"),
     new Date("2026-12-18T00:00:00"),
     new Date("2026-12-31T00:00:00")
 ];
